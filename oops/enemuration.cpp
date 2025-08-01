a939da42c4d41fe802ea5a8363974210888bc567
Karthik Medapalli <maxwhitie1234@gmail.com>
2025-08-01 12:26:57 +0530

    documenting what is enumeration

--- oops/enemuration.cpp
+++ oops/enemuration.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
+/*
+what is enumeration ?
+Enumeration is a user defined data type in which the programmers create their own coustom data type by defining a set of named integer constants
+that have specifice meaning within their program 
 
+for example : 
+instead of using raw integers like 0,1,2 to represent days of week , we can create a user-defined enumuration like {monday , tuesday ,
+wednesday ,thursday , friday , saturday , sunday}
+*/
 enum shape 
 {
     circle,
