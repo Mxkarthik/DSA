90146e504262b1bd8b62443b3581651ce614001c
Karthik <maxwhitie1234@gmail.com>
2025-08-06 23:57:30 +0530

    2D multiplication

--- arrays/matrices/2Dmultiplication.cpp
+++ arrays/matrices/2Dmultiplication.cpp
@@ -1,49 +1,59 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-
-void multiplication (vector <int> array1 , vector <int> array2 , int size1 ,int size2)
-{
-
-    if ( m != y) 
-    {
-        cout<<"The matrix multiplication is not possible";
-    }
-    int answer = 0;
-    for (int i = 0 ; i <  ;i++)
-    {
-        for (int j = 0 ; j < size ; j++)
-        {
-            answer 
-        }
-    }
-}
-
 int main ()
 {
-    int m ; int n; int size1;
-    int x ; int y; int size2;
-
-    vector <int> array1 (size1);
-    cout<<"Please Enter the values for Matrices 1";
-    for (int i = 0 ; i < m ; i++)
+    int r1 , c1;
+    cin>>r1>>c1;
+    int A [r1] [c1];
+    for ( int i = 0 ; i < r1 ; i++)
     {
-        for (int j = 0 ; j < n ; j++)
+        for ( int j = 0 ; j < c1 ; j++)
         {
-            cin>>array[i] [j];
+            cin>>A[i] [j];
         }
     }
-    vector <int> array2 (size2);
-    cout<<"Please Enter the values for Matrices 2";
-    for (int i = 0 ; i < x ; i++)
+    int r2; int c2;
+    cin>>r2>>c2;
+    int B [r2] [c2];
+
+    for (int i = 0 ; i < r2 ; i++)
     {
-        for ( int j = 0 ; j < y ;j++)
+        for (int j = 0 ; j <c2 ; j++ )
         {
-            cin>>array[i] [j];
+            cin>>B [i] [j];
         }
     }
 
-    multiplication(array1,array2,size1,size2);
+    if (c1 != r2)
+    {
+        cout<<"Matrix Multiplication not possible for this input";
+        return 0;
+    }
+
+    int c [r1] [c2];
+
+    for(int i = 0 ; i < r1 ; i++)
+    {
+        for ( int j = 0 ; j < c2 ; j++)
+        {
+            int value = 0;
+            for ( int k = 0 ; k < r2 ; k++)
+            {
+                value += A [i] [k] * B [k] [j];
+            }
+            c[i][j] = value;
+        }
+        cout<<endl;
+    }
+
+    for ( int i = 0 ; i < r1 ; i++)
+    {
+        for (int j = 0 ; j<c2 ; j++)
+        {
+            cout<<c[i] [j]<<" ";
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
