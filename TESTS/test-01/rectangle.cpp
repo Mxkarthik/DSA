85c1105a592489e971c40669005db48dc4ee7768
Karthik <maxwhitie1234@gmail.com>
2025-08-11 22:59:47 +0530

    test-01

--- TESTS/test-01/rectangle.cpp
+++ TESTS/test-01/rectangle.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 using namespace std;
 
-
-void rectangle ()
-{
-
-}
-
 int main ()
 {
-    cout<<"please enter the rows and columns of the rectangle";
-    int i; int j;
-    cin>>i; cin>>j;
+    cout<<"please enter the rows"<<endl;
+    int rows; cin>>rows;
+    cout<<"Plese enter the columns"<<endl;
+    int columns; cin>>columns;
+
+    for (int i = 1 ; i <= rows ; i++)
+    {
+        for (int j = 1 ; j <= columns ; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
 }
