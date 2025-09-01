f868be9a2765ab70dbd531cb83fba2375818b325
Karthik Medapalli <maxwhitie1234@gmail.com>
2025-09-01 22:01:25 +0530

    initialising commit

---
+++ handbook/tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+#include <bits/stc++.h>
+using namespace std;
+
+int main ()
+{
+    ios::sync_with_stdio(false); // faster because it remove synocrinisation 
+    cin.tie(nullptr);
+    int m;
+
+    if(cin>>m) {
+        cout<< n << '\n'; //prefer n over endl
+    }
+
+    return 0;
+}
