862b1a0fc1aa562715b44f6dc74e61103e9678ef
MxKarthik <maxwhitie1234@gmail.com>
2026-06-13 14:56:16 +0530

    Adjacency Matrix

---
+++ Graph/AdjMat.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main ()
+{
+    int vertex,edges;
+    cout<<"Please enter the value of vertex and edges\n";
+    cin>>vertex>>edges;
+    vector<vector<bool>>AdjMat(vertex,vector<bool>(vertex,0));
+    int u,v;
+    for (int i = 0 ; i < edges ; i++){
+        cin>>u>>v;
+       AdjMat[u] [v] = 1;
+    }
+    
+    for (int i = 0 ; i < vertex ; i++)
+    {
+        for (int j = 0 ; j < vertex ; j++)
+        {
+            cout<<AdjMat[i][j]<<" ";
+        }
+            cout<<endl;
+    }
+    return 0;
+}
