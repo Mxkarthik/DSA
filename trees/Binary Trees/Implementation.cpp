bf4899ddd11b92531507e387b7257db492d6164b
MxKarthik <maxwhitie1234@gmail.com>
2026-06-27 12:38:59 +0530

    Binary Tree Implementation

---
+++ trees/Binary Trees/Implementation.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node 
+{
+    public:
+    int data;
+    Node *left,*right;
+    Node (int value)
+    {
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int main ()
+{
+    cout<<"Please Enter the value of root"<<"\n";
+    int x; cin>>x; 
+    int first,second;
+    Node *root = new Node(x);
+    queue <Node*> q;
+    q.push(root);
+
+    while(!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+        // Left Child 
+        cout<<"Please Enter the value of left child of "<<temp->data<<endl;
+        cin>>first;
+        if ( first != -1)
+        {
+            temp->left = new Node(first);
+            q.push(temp->left);
+        }
+        cout<<"Please Enter the value of right child "<<temp->data<<endl;
+        cin>>second;
+        if ( second != -1)
+        {
+            temp->right = new Node(second);
+            q.push(temp->right);
+        }
+
+    }
+}
